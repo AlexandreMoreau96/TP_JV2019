fa75544e3da66f97ba1324872af5372fde65c3ce
william <wil.beau@hotmail.com>
2019-10-18 11:23:49 -0400

    detection de collision sur le terrain

--- Assets/Script/Volleyball.cs
+++ Assets/Script/Volleyball.cs
@@ -10,7 +10,15 @@ public class Volleyball : MonoBehaviour
     public float Vx = 0;
     public float Vy = 0;
     public float Vz = 0;
+    private bool notCollide = true;
+    private ThrowSimulation m_Throw;
+    private bool m_collisionAPriori;
+    private float m_ElapseTime;
 
+    private void Start()
+    {
+        m_Throw = GameObject.Find("Player1").GetComponent<ThrowSimulation>();
+    }
     public void DetectCollision()
     {
         foreach (GameObject vObject in possibleCollisionGameObjects)
@@ -24,10 +32,14 @@ public class Volleyball : MonoBehaviour
                 }
             }
 
-            if (vObject.tag == "terrain") {
-                if (Vector3.Dot(vObject.GetComponent<Plane>().normal, transform.position) < GetComponent<Renderer>().bounds.extents.magnitude) {
-                    Vy = -Vy * 0.8f;
-                    Debug.Log("La balle frappera le sol");
+            if (vObject.tag == "terrain") { 
+                Vector3 vSize = vObject.GetComponent<Renderer>().bounds.size;
+                if (transform.position.x >= 0 && transform.position.x <= vSize.x && transform.position.z >= 0 && transform.position.z <= vSize.z)
+                {
+                    m_ElapseTime = 0;
+                    Debug.Log("collision avec le terrain");
+                    Vy = -Vy * 0.1f;
+
                 }
             }
         }
@@ -47,18 +59,20 @@ public class Volleyball : MonoBehaviour
                 if (vLastX >= 0 && vLastX <= vSize.x && vLastZ >= 0 && vLastZ <= vSize.z)
                 {
                     Vector3 vContactPosition = new Vector3(vLastX, vObject.transform.position.y, vLastZ);
+                    m_collisionAPriori = true;
                     return true;
                 }
 
                 break;
             }
         }
-
+        m_collisionAPriori = false;
         return false;
     }
 
     void Update()
     {
+        m_ElapseTime += Time.deltaTime;
         DetectCollision();
     }
 }
